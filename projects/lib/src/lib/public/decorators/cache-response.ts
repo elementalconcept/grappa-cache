054e4de664be4b0eafebfe48dffa54c21751c177
Aleksandr Ivanov <aux@elementalconcept.com>
2019-11-14 14:34:48 +0000

    Commented out debug code

--- projects/lib/src/lib/public/decorators/cache-response.ts
+++ projects/lib/src/lib/public/decorators/cache-response.ts
@@ -2,7 +2,7 @@ import { Registry } from '@elemental-concept/grappa';
 
 export function CacheResponse() {
   return (target: any, property: string) => {
-    console.log(target);
-    console.log(property);
+    // console.log(target);
+    // console.log(property);
   };
 }
