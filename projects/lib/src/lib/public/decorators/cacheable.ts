e7b4fb8308a788305bda222dcb6088691edcf13c
Aleksandr Ivanov <aux@elementalconcept.com>
2019-11-15 11:14:26 +0000

    Code clean-up

--- projects/lib/src/lib/public/decorators/cacheable.ts
+++ projects/lib/src/lib/public/decorators/cacheable.ts
@@ -3,7 +3,5 @@ import { Initialisable, Registry } from '@elemental-concept/grappa';
 import { CacheableClient } from '../../internal/cacheable-client';
 
 export function Cacheable() {
-  return (constructor: Initialisable) => {
-    Registry.registerAlternativeHttpClient(constructor.prototype, new CacheableClient());
-  };
+  return (constructor: Initialisable) => Registry.registerAlternativeHttpClient(constructor.prototype, new CacheableClient());
 }
