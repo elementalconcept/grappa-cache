5ce05f34368d10cdf9dc040416b71d84e5908f92
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2020-07-29 17:26:22 +0100

    HOTFIX: fixed public exports

--- projects/lib/src/lib/public/index.ts
+++ projects/lib/src/lib/public/index.ts
@@ -1,4 +1,6 @@
 export * from './decorators/cache-request';
 export * from './decorators/cache-response';
 export * from './decorators/cacheable';
+export * from './persistence/local-storage';
+export * from './persistence/persistence-manager';
 export * from './services/cache-replay.service';
